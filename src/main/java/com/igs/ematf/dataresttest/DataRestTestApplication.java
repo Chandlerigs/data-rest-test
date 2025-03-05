04d2c563e68a8b6f6777f619c456e473cb975415
Chandler Ji <Chandler.Ji@igsl-group.com>
2025-03-05 14:48:43 +0800

    initial commit

---
+++ src/main/java/com/igs/ematf/dataresttest/DataRestTestApplication.java
@@ -0,0 +1,13 @@
+package com.igs.ematf.dataresttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataRestTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataRestTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/igs/ematf/dataresttest/DataRestTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igs.ematf.dataresttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataRestTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
