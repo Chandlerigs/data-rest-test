46312bf5fe5f4ca56b56eb3ade9d1113692f8b5c
Chandler Ji <Chandler.Ji@igsl-group.com>
2025-04-17 09:43:04 +0800

    支持查询关联字段，支持嵌套
    请求链接
    http://localhost:8080/api/user?page=0&size=20&&department.rule.ruleName=11111

--- src/main/java/com/igs/ematf/dataresttest/repository/dsl/BaseDslRepository.java
+++ src/main/java/com/igs/ematf/dataresttest/repository/dsl/BaseDslRepository.java
@@ -1,12 +1,9 @@
 package com.igs.ematf.dataresttest.repository.dsl;
 
 import com.igs.ematf.dataresttest.dto.PermissionCondition;
-import com.igs.ematf.dataresttest.entity.SysDepartment;
-import com.igs.ematf.dataresttest.entity.SysUser;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.EntityPath;
 import com.querydsl.core.types.Path;
-import com.querydsl.core.types.PathType;
 import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.EntityPathBase;
 import com.querydsl.core.types.dsl.NumberPath;
@@ -33,7 +30,7 @@ public interface BaseDslRepository<T, ID, Q extends EntityPath<?>> extends JpaRe
             if ("eq".equals(comparisonOperator)) {
                 return numberPath.eq(fieldValueFormat);
             } else {
-                return numberPath.ne(fieldValueFormat);
+                return numberPath.eq(fieldValueFormat);
             }
         }
         if (path instanceof NumberPath<?> numberPath) {
@@ -61,10 +58,13 @@ public interface BaseDslRepository<T, ID, Q extends EntityPath<?>> extends JpaRe
         // 解析行权限。转化为binding
         List<PermissionCondition> permissions = getPermissions(root);
         for (PermissionCondition permission : permissions) {
+            String tableName = permission.getTableName();
             String fieldName = permission.getFieldName();
+            List<String> fieldNameList = new ArrayList<>(Arrays.asList(tableName.split("\\.")));
+            fieldNameList.add(fieldName);
             String comparisonOperator = permission.getComparisonOperator();
             String fieldValue = permission.getComparisonData();
-            Path<?> fieldSafely = getFieldSafely(root, fieldName);
+            Path<?> fieldSafely = getFieldWithRelationSafely(root, fieldNameList);
             if (fieldSafely == null) {
                 continue;
             }
@@ -76,6 +76,7 @@ public interface BaseDslRepository<T, ID, Q extends EntityPath<?>> extends JpaRe
 
     }
 
+
     /**
      * 模拟行权限
      * @param root 当前Q类
@@ -84,29 +85,66 @@ public interface BaseDslRepository<T, ID, Q extends EntityPath<?>> extends JpaRe
     private List<PermissionCondition> getPermissions(Q root) {
         // 根据用户信息和当前访问实体类，获取用户行权限信息
         List<PermissionCondition> permissions = new ArrayList<>();
-        permissions.add(new PermissionCondition("department", "gt", "5"));
+        PermissionCondition condition = getPermissionCondition();
+        permissions.add(condition);
         return permissions;
     }
 
-    private Path<?> getFieldSafely(Q root, String fieldName) {
+    private PermissionCondition getPermissionCondition() {
+        PermissionCondition condition = new PermissionCondition();
+        condition.setTableName("department.rule");
+        condition.setFieldName("ruleName");
+        condition.setComparisonData("bbb");
+        condition.setComparisonOperator("=");
+        condition.setComparisonDataType(String.class);
+        return condition;
+    }
+
+    private PermissionCondition getPermissionCondition2() {
+        PermissionCondition condition = new PermissionCondition();
+        condition.setTableName("department");
+        condition.setFieldName("rule");
+        condition.setComparisonData("1");
+        condition.setComparisonOperator("=");
+        condition.setComparisonDataType(Long.class);
+        return condition;
+    }
+
+    private Path<?> getFieldSafely(Object root, List<String> fieldNameList) {
+
+        String fieldItemName = fieldNameList.get(0);
         try {
-            Field field = root.getClass().getDeclaredField(fieldName);
+            Field field = root.getClass().getDeclaredField(fieldItemName);
             field.setAccessible(true);
             Object rootField = field.get(root);
-            if (field.getType().getSuperclass() == EntityPathBase.class) {
-                // 如果字段是 EntityPathBase 的子类，则返回其 id 字段
-                Field idField = rootField.getClass().getDeclaredField("id"); // 获取 id 字段
-                idField.setAccessible(true); // 确保可以访问私有字段
-                return (Path<?>) idField.get(rootField); // 返回 id 字段
-            } else {
+            if (field.getType().getSuperclass() != EntityPathBase.class) {
                 return (Path<?>) rootField;
             }
+            List<String> nextLeveFieldNameList = fieldNameList.subList(1, fieldNameList.size());
+            // 如果字段是 EntityPathBase 的子类，则返回其 id 字段
+            Field idField = rootField.getClass().getDeclaredField("id"); // 获取 id 字段
+            idField.setAccessible(true); // 确保可以访问私有字段
+            return getFieldSafely(idField, nextLeveFieldNameList); // 返回 id 字段
+
         } catch (NoSuchFieldException | IllegalAccessException e) {
             System.out.println("QueryDSL 获取字段类型失败, " +
                     "类型：" + root.getClass() +
-                    "，字段名称：" + fieldName +
+                    "，字段名称：" + fieldItemName +
                     "，错误信息：" + e.getMessage());
         }
         return null;
+    }
+
+    private Path<?> getFieldWithRelationSafely(Object root, List<String> fieldNameList) {
+        try {
+            for (String fieldItemName : fieldNameList) {
+                Field field = root.getClass().getDeclaredField(fieldItemName);
+                field.setAccessible(true);
+                root = field.get(root);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return (Path<?>) root;
     }
 }
